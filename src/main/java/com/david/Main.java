99b3fad79057c2dc8d169a040365165b0df330ab
KalachyanD <kalachyand@gmail.com>
2024-01-06 23:58:57 +0400

    Code enhancements

--- src/main/java/com/david/Main.java
+++ src/main/java/com/david/Main.java
@@ -1,77 +1,18 @@
 package com.david;
 
-import java.io.InputStream;
-
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.Random;
-
 public class Main {
     public static void main(String[] args){
 
-        String dictionaryPathJar = "dictionary.csv";
+        final String dictionaryPathInJar = "dictionary.csv";
 
         try{
-            List<IrregularVerb> allVerbs = getAllIrregularVerbsFromJar(dictionaryPathJar);
+            VerbTrainer verbTrainer = new VerbTrainer(dictionaryPathInJar);
 
-            IrregularVerb randomIrregularVerb = getRandomIrregularVerb(allVerbs);
-
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Infinitive: " + randomIrregularVerb.getInfinitive());
-            System.out.print("Past: ");
-            String pastInput = scanner.nextLine();
-            System.out.print("Past Participle: ");
-            String pastParticipleInput = scanner.nextLine();
-
-            IrregularVerb inputVerb = new IrregularVerb(randomIrregularVerb.getInfinitive(), pastInput, pastParticipleInput);
-
-            if(randomIrregularVerb.equals(inputVerb)){
-                System.out.print("Correct\n");
-            }else{
-                System.out.print("Incorrect\n");
-            }
+            verbTrainer.startPractise();
 
         }catch (Exception e){
             System.out.println("Something went wrong.");
         }
 
-    }
-
-    private static List<IrregularVerb> getAllIrregularVerbsFromJar(String filePath){
-        List<IrregularVerb> irregularVerbs = new ArrayList<>();
-
-        ClassLoader classLoader = Main.class.getClassLoader();
-
-        try (InputStream inputStream = classLoader.getResourceAsStream(filePath);
-             Scanner scanner = new Scanner(inputStream)) {
-
-            String line;
-            String[] array;
-
-            while (scanner.hasNextLine()) {
-                line = scanner.nextLine();
-                array = line.split(",");
-                irregularVerbs.add(new IrregularVerb(array[0], array[1], array[2]));
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return irregularVerbs;
-    }
-
-    private static IrregularVerb getRandomIrregularVerb(List<IrregularVerb> irregularVerbList){
-        if(irregularVerbList != null && irregularVerbList.size() != 0){
-            if(irregularVerbList.size() > 1){
-                Random random = new Random();
-                return irregularVerbList.get(random.nextInt(irregularVerbList.size()));
-            }else{
-                return irregularVerbList.get(0);
-            }
-        }else{
-            return null;
-        }
     }
 }
---
+++ src/main/java/com/david/VerbTrainer.java
@@ -0,0 +1,86 @@
+package com.david;
+
+import java.io.InputStream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class VerbTrainer {
+    private final List<IrregularVerb> irregularVerbs;
+    private final Random randomizer;
+
+    public VerbTrainer(String filePath) {
+        this.irregularVerbs = this.getAllIrregularVerbsFromJar(filePath);
+        this.randomizer = new Random();
+    }
+
+    private List<IrregularVerb> getAllIrregularVerbsFromJar(String filePath) {
+
+        List<IrregularVerb> irregularVerbs = new ArrayList<>();
+        ClassLoader classLoader = Main.class.getClassLoader();
+
+        try (InputStream inputStream = classLoader.getResourceAsStream(filePath);
+             Scanner scanner = new Scanner(inputStream)) {
+
+            String line;
+            String[] array;
+
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+                array = line.split(",");
+
+                if (array.length == 3) {
+                    irregularVerbs.add(new IrregularVerb(array[0], array[1], array[2]));
+                } else {
+                    throw new Exception("Invalid CSV line " + irregularVerbs.size() + 1 + ".");
+                }
+            }
+
+            if(irregularVerbs.size() == 0){
+                throw new Exception("CSV file is empty or broken.");
+            }
+
+        } catch (Exception e) {
+            System.err.print(e.getMessage());
+        }
+
+        return irregularVerbs;
+    }
+
+    private IrregularVerb getRandomIrregularVerb() {
+
+        if(irregularVerbs.size() > 1){
+            return irregularVerbs.get(randomizer.nextInt(irregularVerbs.size()));
+        }else{
+            return irregularVerbs.get(0);
+        }
+    }
+
+    public void startPractise(){
+
+        //getting one random verb
+        IrregularVerb randomIrregularVerb = this.getRandomIrregularVerb();
+
+        //interaction with user logic
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Infinitive: " + randomIrregularVerb.getInfinitive());
+        System.out.print("Past: ");
+        String pastInput = scanner.nextLine();
+        System.out.print("Past Participle: ");
+        String pastParticipleInput = scanner.nextLine();
+
+        //checking of equality inserted verb forms and reference ones
+        IrregularVerb inputVerb = new IrregularVerb(randomIrregularVerb.getInfinitive(), pastInput, pastParticipleInput);
+
+        //result output for user
+        if(randomIrregularVerb.equals(inputVerb)){
+            System.out.print("Correct\n");
+        }else{
+            System.out.print("Incorrect\n");
+        }
+    }
+
+}
+
